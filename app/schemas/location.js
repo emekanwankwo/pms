0e447fa0a8c5645b36e2369c7d47f825e36addc1
Ethan <emekanwankwo201@gmail.com>
2018-03-26 11:38:27 +0100

    Update schema to accept only integers

--- app/schemas/location.js
+++ app/schemas/location.js
@@ -3,15 +3,43 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const locationSchema = new Schema({
-    male_residents: {type: Number, required: true},
-    female_residents: {type: Number, required: true},
+    male_residents: {
+        type: Number,
+        required: true,
+        validate : {
+            validator : Number.isInteger,
+            message   : 'male_residents must be an integer value'
+        }
+    },
+    female_residents: {
+        type: Number,
+        required: true,
+        validate : {
+            validator : Number.isInteger,
+            message   : 'female_residents must be an integer value'
+        }
+    },
     created_at: {type: Date, default: new Date()},
     updated_at: {type: Date, default: new Date()}
 });
 
 const locationDataSchema = new Schema({
-    male_residents: {type: Number, required: true},
-    female_residents: {type: Number, required: true},
+    male_residents: {
+        type: Number,
+        required: true,
+        validate : {
+            validator : Number.isInteger,
+            message   : 'male_residents must be an integer value'
+        }
+    },
+    female_residents: {
+        type: Number,
+        required: true,
+        validate : {
+            validator : Number.isInteger,
+            message   : 'female_residents must be an integer value'
+        }
+    },
     total_residents: {type: Number},
     locations: [locationSchema],
     created_at: {type: Date, default: new Date()},
