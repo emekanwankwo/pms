56af9063c39bf62ca5098551d455efa90dccb2a3
Ethan <emekanwankwo201@gmail.com>
2018-03-26 11:25:50 +0100

    Add total residents

--- app/schemas/location.js
+++ app/schemas/location.js
@@ -12,6 +12,7 @@ const locationSchema = new Schema({
 const locationDataSchema = new Schema({
     male_residents: {type: Number, required: true},
     female_residents: {type: Number, required: true},
+    total_residents: {type: Number},
     locations: [locationSchema],
     created_at: {type: Date, default: new Date()},
     updated_at: {type: Date, default: new Date()}
